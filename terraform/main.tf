c0711cf4705e3018a1007e810dfa51880f45df85
kaeru <soccerxxkaeru@gmail.com>
2022-11-11 20:33:33 +0900

    create lambda function

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,6 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 
-  profile = "localstack"
-
   access_key = "mock_access_key"
   secret_key = "mock_secret_key"
 
@@ -18,9 +16,9 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    iam     = "http://localhost:4566"
-    kinesis = "http://localhost:4566"
-    lambda  = "http://localhost:4566"
-    s3      = "http://localhost:4566"
+    iam     = "http://localstack:4566"
+    kinesis = "http://localstack:4566"
+    lambda  = "http://localstack:4566"
+    s3      = "http://localstack:4566"
   }
 }
